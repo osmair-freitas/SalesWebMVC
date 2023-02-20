56fe14d7177c9054fd5ce8ea832b91c081995d95
Osmair Freitas <osmairfreitas@gmail.com>
2023-02-20 14:46:06 -0300

    Other entities and second migration

--- SalesWebMVC/Controllers/DepartmentsController.cs
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMVC.Data;
+using SalesWebMVC.Models;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Controllers
